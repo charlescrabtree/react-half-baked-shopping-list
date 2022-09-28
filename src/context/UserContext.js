1dad1db6d3e7b306d6ca593f9cc2d2965e16f2a1
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-09-28 16:06:54 -0700

    added semicolon to UserContext.js

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -7,7 +7,7 @@ const UserProvider = ({ children }) => {
   const currentUser = getUser();
   const [user, setUser] = useState(currentUser);
   
-  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
 };
 
 export { UserProvider, UserContext };
